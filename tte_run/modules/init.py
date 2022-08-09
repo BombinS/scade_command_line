3bc4e4c96489aae08d16d0e96834b2bb6dc5db1a
Sergey Bombin <sergey.bombin.1977@gmail.com>
2022-08-09 16:18:33 +0300

    tee_run clean up previous results

--- tte_run/modules/init.py
+++ tte_run/modules/init.py
@@ -1,4 +1,4 @@
-import os, logging
+import os, logging, shutil
 import config
 
 def cleanup_and_setup():
@@ -8,7 +8,16 @@ def cleanup_and_setup():
     if (os.path.exists(log_filename)):   
         os.remove(log_filename)
 
+    # удаление предыдущих результатов
+    for root, dirs, files in os.walk(config.path_to_test_folder):
+        for dir in dirs:
+            if '_Result' in dir:
+                shutil.rmtree(os.path.join(root,dir))
+
     # инициализация логера
     logging.basicConfig(filename=log_filename, level=logging.DEBUG)
+
+    
+
 
 
