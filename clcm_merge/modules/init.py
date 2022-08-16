dd7cb208ec46d3bf68f78462d4615b8eda00d9a0
Sergey Bombin <sergey.bombin.1977@gmail.com>
2022-08-16 21:19:52 +0300

    clmc run

---
+++ clcm_merge/modules/init.py
@@ -0,0 +1,23 @@
+import os, logging, shutil
+import config
+
+def cleanup_and_setup():
+    
+    # удаление предыдущего лога
+    log_filename = config.tte_run_log
+    if (os.path.exists(log_filename)):   
+        os.remove(log_filename)
+
+    # удаление предыдущих результатов
+    # for root, dirs, files in os.walk(config.path_to_test_folder):
+    #     for dir in dirs:
+    #         if '_Result' in dir:
+    #             shutil.rmtree(os.path.join(root,dir))
+
+    # инициализация логера
+    logging.basicConfig(filename=log_filename, level=logging.DEBUG)
+
+    
+
+
+
---
+++ clcm_merge/modules/modify.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as et
+import config
+
+def modify_ept_file(path):
+    
+    tree = et.parse(path)
+    root = tree.getroot()
+    
+    element = root.findall(".//*[@name='@QTE:SOURCE_MODEL']/value")[0]
+    element.text = config.path_to_root_model
+    
+    element = root.findall(".//*[@name='Model']")[0]
+    el = element.findall(".//*FileRef")[0]
+    el.set('persistAs', config.path_to_root_model)
+
+    tree.write(path)
+    
+    return
